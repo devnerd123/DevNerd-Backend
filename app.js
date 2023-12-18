223a1e5d817f611548e760e7c79bb1eafe3b38a6
irfan106 <ir.irfan106@gmail.com>
2023-12-18 09:29:49 +0530

    company-schema-updated

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const graphQLSchema = require('./graphql/schema/index')
 const graphQLResolvers = require('./graphql/resolvers/index')
-const isAuth = require("./middleware/isAuth")
-
+const cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use(isAuth)
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 
 app.use('/api-graph', graphqlHTTP({
     schema: graphQLSchema,
